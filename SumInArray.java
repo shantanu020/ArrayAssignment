cd9d6d26a1cecc14893fe75a1de0924c81742659
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-01-13 20:20:08 +0530

    Update SumInArray.java
    Ans 3

--- SumInArray.java
+++ SumInArray.java
@@ -2,7 +2,7 @@
 public class SumInArray {
 
 	public static void main(String[] args) {
-		//Ans 5: Program to calculate sum of all elements of an integer array
+		//Ans 3: Program to calculate sum of all elements of an integer array
 		int[] array=new int[] {-3,-2,-1,0,1,2,3,4};
 		int sum=0;
 		for(int i : array) {
